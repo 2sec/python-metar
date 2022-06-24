48cbacf70c1963de2ed3868b14ba6ba86f2dfa4a
yannik <30745185+2sec@users.noreply.github.com>
2022-06-24 15:59:52 +0200

    GAE testing

--- dataset.py
+++ dataset.py
@@ -11,17 +11,17 @@ import time
 import Log
 import utils
 
-import gc
-import io
 
 
 
 debug = False
 
 
+def file_exist(filename):
+    return os.path.isfile(filename)
+
 def is_older_than(filename, minutes):
-    if not os.path.isfile(filename):
-        return True
+    if not file_exist(filename): return True
 
     modify_date = datetime.fromtimestamp(os.path.getmtime(filename))
     
@@ -29,7 +29,7 @@ def is_older_than(filename, minutes):
 
 
 
-def download_aviationweather_csv(filename, debug_header_size = 5):
+def download_aviationweather_csv(filename, only_read_existing = False, debug_header_size = 5):
     base_url = 'https://www.aviationweather.gov/adds/dataserver_current/current/'
 
     input_filename = filename + '.cache.csv'   #no need to request the gz version, the service returns it zipped anyway and the requests lib unzips it transparently
@@ -38,53 +38,61 @@ def download_aviationweather_csv(filename, debug_header_size = 5):
 
     output_list = []
 
-    #as multiple processes can be spawned, the files are no longer saved. 
-    #TODO move download to some separate task
-    #if is_older_than('download/' + input_filename, minutes=2.5): 
-    Log.Write('downloading from %s' % url)
-    response = requests.get(url)
-    Log.Write('response status code = ' + str(response.status_code))
-    if response.status_code != 200:
-        raise Exception("Invalid status code")
+    if only_read_existing and not file_exist('download/' + input_filename):
+        return []
 
-    response = response.content.decode('utf-8')
-    #default debug header:
-    #No errors
-    #No warnings
-    #474 ms
-    #data source=metars
-    #4809 results
 
-    n = -1
-    for i in range(0,5):
-        n = response.index('\n', n+1)
-    debug_header = response[0:n]
+    if only_read_existing:
+        with open('download/' + output_filename, 'r', newline='\n', encoding='utf-8') as f:
+            text = f.read()
+            output_list = text.split('\n')
+            if output_list[-1] == '': output_list.pop()
 
-    response = response[n+1:]
 
-    Log.Write(debug_header)
+    if not only_read_existing and is_older_than('download/' + input_filename, minutes=2.5): 
+        Log.Write('downloading from %s' % url)
+        response = requests.get(url)
+        Log.Write('response status code = ' + str(response.status_code))
+        if response.status_code != 200:
+            raise Exception("Invalid status code")
 
-    #with open('download/' + input_filename, 'wb') as f:
-    #    f.write(response.encode('utf-8'))
+        response = response.content.decode('utf-8')
+        #default debug header:
+        #No errors
+        #No warnings
+        #474 ms
+        #data source=metars
+        #4809 results
 
-    # extract raw_text
-    #with open('download/' + input_filename, 'r', encoding='utf-8') as f, open('download/' + output_filename, 'w', newline='\n', encoding='utf-8') as f_out:
-    #rows = csv.reader(f)
-    rows = csv.reader(io.StringIO(response))
+        n = -1
+        for i in range(0,5):
+            n = response.index('\n', n+1)
+        debug_header = response[0:n]
 
-    #skip header row
-    next(rows)
+        response = response[n+1:]
 
-    for row in rows:
-        output_list.append(row[0])
-        #f_out.write('%s\n' % row[0])
+        Log.Write(debug_header)
+
+        with open('download/' + input_filename, 'wb') as f:
+            f.write(response.encode('utf-8'))
+
+        #extract raw_text
+        with open('download/' + input_filename, 'r', encoding='utf-8') as f, open('download/' + output_filename, 'w', newline='\n', encoding='utf-8') as f_out:
+            rows = csv.reader(f)
+
+            #skip header row
+            next(rows)
+
+            for row in rows:
+                output_list.append(row[0])
+                f_out.write('%s\n' % row[0])
 
     output_list.sort()
     return output_list
 
 
 
-def download_ourairports_csv(filename):
+def download_ourairports_csv(filename, only_read_existing = False):
     base_url = 'https://davidmegginson.github.io/ourairports-data/'
 
     input_filename = filename + '.csv'    
@@ -92,88 +100,119 @@ def download_ourairports_csv(filename):
 
     output_list = []
 
-    #if is_older_than('download/' + input_filename, minutes=60): 
-    Log.Write('downloading from %s' % url)
-    response = requests.get(url)
-    Log.Write('response status code = ' + str(response.status_code))
-    if response.status_code != 200:
-        raise Exception("Invalid status code")
 
-    response = response.content.decode('utf-8')
-    #with open('download/' + input_filename, 'wb') as f:
-    #    f.write(response.content)
+    if only_read_existing and not file_exist('download/' + input_filename):
+        return []
+
+    if not only_read_existing and is_older_than('download/' + input_filename, minutes=60): 
+        Log.Write('downloading from %s' % url)
+        response = requests.get(url)
+        Log.Write('response status code = ' + str(response.status_code))
+        if response.status_code != 200:
+            raise Exception("Invalid status code")
+
+        with open('download/' + input_filename, 'wb') as f:
+            f.write(response.content)
 
 
-    #with open('download/' + input_filename, 'r', encoding='utf-8') as f:
-    #rows = csv.DictReader(f)
-    rows = csv.DictReader(io.StringIO(response))
+    with open('download/' + input_filename, 'r', encoding='utf-8') as f:
+        rows = csv.DictReader(f)
+        output_list = list(rows)
 
-    output_list = list(rows)
 
     return output_list
 
 
-airports = []
-runways = []
-airport_idents = []
-airport_names = []
+
+class Cache(object):
+    def __init__(self):
+        self.airports = []
+        self.runways = []
+        self.metars = []
+        self.tafs = []
+        self.airport_idents = []
+        self.airport_names = []
+        self.date = 0 # todo: do not reload dataset if not changed
+
+    def download(self, only_read_existing = False):
+        self.airports = download_ourairports_csv('airports', only_read_existing = only_read_existing)
+        self.runways = download_ourairports_csv('runways', only_read_existing = only_read_existing)
+        self.metars = download_aviationweather_csv('metars', only_read_existing = only_read_existing)
+        self.tafs = download_aviationweather_csv('tafs', only_read_existing = only_read_existing)
+
+    def update(self):
+
+        self.download(True)
+
+        tafs = self.tafs
+        metars = self.metars
+
+        #inconsistency in the source - sometimes a TAF starts with the word TAF, sometimes not
+        for i, taf in enumerate(tafs):
+            if taf.startswith('TAF '): tafs[i] = taf[4:]
+        tafs.sort()
+
+        idents = []
+        names = []
+
+        # advance in all sorted lists at the same time, and find matching metars, tafs and runways
+        metars_iter = iter(metars)
+        metar = next(metars_iter, None)
 
 
-#download all datasets continously in the background
-def download(sleep = True):
-    global airports
-    global runways
-    global airport_idents
-    global airport_names
+        tafs_iter = iter(tafs)
+        taf = next(tafs_iter, None)
 
 
-    airports = download_ourairports_csv('airports')
-    runways = download_ourairports_csv('runways')
-    metars = download_aviationweather_csv('metars')
-    tafs = download_aviationweather_csv('tafs')
+        for airport in self.airports:
+            ident = airport['ident'].upper()
+            idents.append(ident)
+            names.append(airport['name'])
 
-    #inconsistency in the source - sometimes a TAF starts with the word TAF, sometimes not
-    for i, taf in enumerate(tafs):
-        if taf.startswith('TAF '): tafs[i] = taf[4:]
-    tafs.sort()
-
-    idents = []
-    names = []
-
-    # advance in all sorted lists at the same time, and find matching metars, tafs and runways
-    metars_iter = iter(metars)
-    metar = next(metars_iter, None)
-    tafs_iter = iter(tafs)
-    taf = next(tafs_iter, None)
+            # find matching metar, taf or runways, if any
+            airport['metar'] = None
+            airport['taf'] = None
 
 
-    for airport in airports:
-        ident = airport['ident'].upper()
-        idents.append(ident)
-        names.append(airport['name'])
+            while metar and ident > metar[0:4]: metar = next(metars_iter, None)
+            if metar and ident == metar[0:4]: airport['metar'] = metar
 
-        # find matching metar, taf or runways, if any
-        airport['metar'] = None
-        airport['taf'] = None
+            while taf and ident > taf[0:4]: taf = next(tafs_iter, None)
+            if taf and ident == taf[0:4]: airport['taf'] = taf
+        
 
-        while metar and ident > metar[0:4]: metar = next(metars_iter, None)
-        if metar and ident == metar[0:4]: airport['metar'] = metar
-
-        while taf and ident > taf[0:4]: taf = next(tafs_iter, None)
-        if taf and ident == taf[0:4]: airport['taf'] = taf
-    
-
-    airport_idents = idents
-    airport_names = names
-    
-    gc.collect()
+        self.airport_idents = idents
+        self.airport_names = names
 
 
-    time.sleep(60 * 2.5)
+cache = Cache()
+
+#update in memory datasets every 30 seconds in the background
 
 
+#todo bug: if download and update run at the same time (in the same process or not)
+def update(firstTime = False, sleep = True):
+    if sleep:
+        time.sleep(30)
 
-utils.StartThread(download, 'download', restartOnException=True)
+    new_cache = Cache()
+    new_cache.update()
+    global cache
+    cache = new_cache
+
+    if firstTime:
+        utils.StartThread(update, 'update', restartOnException=True)
+
+
+#initialize dataset
+update(True, False)
+
+   
+
+# called every 2.5 minutes by GAE: /tasks/download
+def download():
+    Cache().download()
+
 
 
 
@@ -184,7 +223,10 @@ if __name__ == '__main__':
     #config = CamConfig()
     #config.Run()
 
-    while True: time.sleep(1000)
+    while True:
+        download()
+        time.sleep(1000)
+
 
 
 
