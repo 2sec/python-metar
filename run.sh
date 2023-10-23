3b7d1afa08c4b918a0064dc972df0c76f50973c7
yannik <30745185+2sec@users.noreply.github.com>
2023-10-23 12:52:41 +0200

    added run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$HOME/.local/bin/gunicorn --workers 1 --thread 16 --bind :8000 --access-logfile - --access-logformat "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '(f)s' '%(a)s' %(M)s" app:app 
